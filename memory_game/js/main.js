b3637fa9b3d552b80d3401b23db6689d80657f2e
Brody Currie <brodycurrie@Brody-Curries-MacBook-Pro-2.local>
2019-04-18 13:03:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,6 +20,7 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -30,17 +31,38 @@ var checkForMatch = function() {
 	};
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	};
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+var reset = function() {
+	cardsInPlay = [];
+	document.getElementById('game-board').innerHTML = "";
+	createBoard();
+}
+
+document.getElementById('reset').addEventListener('click', reset);
+
+createBoard();
+
+
